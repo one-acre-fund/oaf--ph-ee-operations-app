3e37e928a0b0d140ee87f1fecbc1efaa7122e2d3
Vikeln <vikelnthuku@gmail.com>
2026-04-08 08:29:30 +0300

    feature: remove existing role permissions

--- src/main/resources/sql/migrations/tenant/V48__Add_permissions_for_roles.sql
+++ src/main/resources/sql/migrations/tenant/V48__Add_permissions_for_roles.sql
@@ -5,12 +5,6 @@ insert into m_permission (code, grouping, entity_name, action_name, can_maker_ch
 values ('CREATE_ROLE', 'authorisation', 'ROLE', 'CREATE', 0);
 
 insert into m_permission (code, grouping, entity_name, action_name, can_maker_checker)
-values ('DISABLE_ROLE', 'authorisation', 'ROLE', 'DISABLE', 0);
-
-insert into m_permission (code, grouping, entity_name, action_name, can_maker_checker)
-values ('ENABLE_ROLE', 'authorisation', 'ROLE', 'ENABLE', 0);
-
-insert into m_permission (code, grouping, entity_name, action_name, can_maker_checker)
 values ('UPDATE_ROLE', 'authorisation', 'ROLE', 'UPDATE', 0);
 
 insert into m_permission (code, grouping, entity_name, action_name, can_maker_checker)
